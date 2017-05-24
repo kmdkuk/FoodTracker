e8cff64d1609198e3de1488400aa1ec0cc95ad18
kouki <kouworld0123@gmail.com>
2017-05-24 13:38:22 +0900

    connect textfield

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
