2d9e199a05a8298578632741e7486a7c25190dd4
kouki <kouworld0123@gmail.com>
2017-05-25 19:30:21 +0900

    return the number of rows in meals

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -33,8 +33,7 @@ class MealTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return meals.count
     }
 
     /*
