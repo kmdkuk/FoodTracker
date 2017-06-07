15874b4dc1535bd5d0e322bd13281d9cc0969213
kouki <kouworld0123@gmail.com>
2017-06-07 10:04:47 +0900

    implement the method to load the meal list

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -153,6 +153,10 @@ class MealTableViewController: UITableViewController {
         }
     }
     
+    private func loadMeals() -> [Meal]?  {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+    }
+    
     //MARK: Actions
     
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
